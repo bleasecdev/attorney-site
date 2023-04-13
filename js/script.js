02321a720f74f97b14b2acff092ba47f84bde4b7
Blease Carson <bleasecarson@gmail.com>
2023-04-12 22:24:27 -0400

    Website Updates.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,9 @@
 $(window).scroll(function() {
-    $('.scroll-slide-left').each(function() {
-      var slideLeftPos = $(this).offset().left + $(this).outerWidth();
-      var windowPos = $(window).scrollLeft() + $(window).width();
-      if (windowPos > slideLeftPos) {
-        $(this).addClass('slide-in-left');
-      }
-    });
-    $('.scroll-slide-right').each(function() {
-      var slideRightPos = $(this).offset().left;
-      var windowPos = $(window).scrollLeft();
-      if (windowPos < slideRightPos) {
-        $(this).addClass('slide-in-right');
+    $(".services-container img").each(function() {
+      var position = $(this).offset().top;
+      var windowTop = $(window).scrollTop() + $(window).height() * 0.8;
+      if (position < windowTop) {
+        $(this).addClass("active");
       }
     });
   });
